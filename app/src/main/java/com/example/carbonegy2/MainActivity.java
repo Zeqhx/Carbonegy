f1b714d629647fb39df2463d80866db18bb50dc2
Zeqhx <s2122641@siswa.um.edu.my>
2023-01-05 04:02:09 +0800

    second

--- app/src/main/java/com/example/carbonegy2/MainActivity.java
+++ app/src/main/java/com/example/carbonegy2/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.carbonegy2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this, R.id.fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        BottomNavigationView a=(BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        a.getMenu().getItem(2).setEnabled(false);
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new recordFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment,fragment);
+                                fragmentTransaction.commit();
+
+
+
+
+
+
+
+
+
+
+            }
+        });
+
+
+
+
     }
 }
