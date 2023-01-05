0f72c1afe0fabcb05633cfe173d72b96c9dd9060
Zeqhx <s2122641@siswa.um.edu.my>
2023-01-06 00:36:57 +0800

    second

--- app/src/main/java/com/example/carbonegy2/SecondFragment.java
+++ app/src/main/java/com/example/carbonegy2/SecondFragment.java
@@ -1,64 +1,107 @@
 package com.example.carbonegy2;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.InputFilter;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SecondFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
+import com.example.carbonegy2.R;
+
 public class SecondFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SecondFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SecondFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SecondFragment newInstance(String param1, String param2) {
-        SecondFragment fragment = new SecondFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    // Declare a context object
+    Context context;
+    int inputNumber;
+    TextView textView;
+    SharedPreferences sharedPreferences;  // Declare a shared preferences object
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        context = getActivity();
+        sharedPreferences = context.getSharedPreferences("com.example.carbonegy2", Context.MODE_PRIVATE);
+        inputNumber = sharedPreferences.getInt("input_number", 0);
+        textView = view.findViewById(R.id.card);
+        textView.setText("Your current goal is to reduce emissions by: " + String.valueOf(inputNumber) + "%");
+
+
+
+
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNumberInputDialog();
+            }
+        });
+
+        return view;
+    }
+
+    public void showNumberInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Enter a number");
+
+        // Set up the input
+        final EditText input = new
+                EditText(context);
+        // Specify the type of input expected; this, for example, sets the input as a number
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter.LengthFilter(3);  // Set the max length to 3
+        input.setFilters(filters);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String inputValue = input.getText().toString();
+                if (inputValue.isEmpty()) {
+                    // Show an error message if the input value is empty
+                    Toast.makeText(context, "Enter a number between 1 and 100", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                inputNumber =  Integer.parseInt(inputValue);
+
+
+                if (inputNumber >= 1 && inputNumber <= 100) {
+
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putInt("input_number", inputNumber);
+                    editor.apply();
+                    // Get the input value
+
+                    textView.setText("Your current goal is to reduce emissions by: " + String.valueOf(inputNumber) + "%");
+                    // Do something with the input value
+                } else {
+                    Toast.makeText(context, "Enter a number between 1 and 100", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
+
