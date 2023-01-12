937b8240b9005b22d714a44bb59f86a8a892ec05
qeelahanaffee <82371568+qeelahanaffee@users.noreply.github.com>
2023-01-13 01:56:34 +0800

    Aqeela

--- app/src/main/java/com/example/carbonegy2/FirstFragment.java
+++ app/src/main/java/com/example/carbonegy2/FirstFragment.java
@@ -1,19 +1,30 @@
 package com.example.carbonegy2;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
+    private MyDBHelper dbHelper;
+    private TextView record1,record2,record3,record4,record5,record6;
+    private int userId;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -38,13 +49,30 @@ public class FirstFragment extends Fragment {
 
 
 
-        // TODO: Set the adapter for the ListView
 
-        // Find the TextView and set its text
-        TextView textView = view.findViewById(R.id.text_view);
-        textView.setText("Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun Fact Fun");
+        record1 = view.findViewById(R.id.Record1);
+        record2 = view.findViewById(R.id.Record2);
+        record3 = view.findViewById(R.id.Record3);
+        record4 = view.findViewById(R.id.Record4);
+        record5 = view.findViewById(R.id.Record5);
+        record6 = view.findViewById(R.id.Record6);
+
+        dbHelper = new MyDBHelper(getActivity());
+
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        String email = sharedPref.getString("email", "default_email");
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.query("users", new String[]{"id"}, "email = ?", new String[]{email}, null, null, null);
+        if (cursor.moveToFirst()) {
+            userId = cursor.getInt(0);
+        }
+        cursor.close();
 
 
         return view;
     }
+
+
+
+
 }
--- app/src/main/java/com/example/carbonegy2/ForthFragment.java
+++ app/src/main/java/com/example/carbonegy2/ForthFragment.java
@@ -150,4 +150,3 @@ public class ForthFragment extends Fragment {
 
 
 }
-
