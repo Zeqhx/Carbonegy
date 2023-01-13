6989ba63f584341517b459b91b09277194ac287e
izzi156 <izziainiwaizah@gmail.com>
2023-01-13 14:24:54 +0800

    Latest homepage with recent record ui

--- app/src/main/java/com/example/carbonegy2/FirstFragment.java
+++ app/src/main/java/com/example/carbonegy2/FirstFragment.java
@@ -23,9 +23,11 @@ import java.util.List;
 public class FirstFragment extends Fragment {
 
     RecyclerView rv;
+    RecyclerView rv2;
     ArrayList<String> dataSource;
     LinearLayoutManager linearLayoutManager;
     MyRvAdapter myRvAdapter;
+    MyRvAdapter2 myRvAdapter2;
 
     private MyDBHelper dbHelper;
     private TextView record1,record2,record3,record4,record5,record6;
@@ -53,15 +55,12 @@ public class FirstFragment extends Fragment {
         progressBar.setMax(100);
         progressBar.setProgress(progress);
 
-
-
-
-        record1 = view.findViewById(R.id.Record1);
-        record2 = view.findViewById(R.id.Record2);
-        record3 = view.findViewById(R.id.Record3);
-        record4 = view.findViewById(R.id.Record4);
-        record5 = view.findViewById(R.id.Record5);
-        record6 = view.findViewById(R.id.Record6);
+//        record1 = view.findViewById(R.id.Record1);
+//        record2 = view.findViewById(R.id.Record2);
+//        record3 = view.findViewById(R.id.Record3);
+//        record4 = view.findViewById(R.id.Record4);
+//        record5 = view.findViewById(R.id.Record5);
+//        record6 = view.findViewById(R.id.Record6);
 
         dbHelper = new MyDBHelper(getActivity());
 
@@ -74,6 +73,7 @@ public class FirstFragment extends Fragment {
         }
         cursor.close();
 
+        //horizontal recyclerview
         rv = view.findViewById(R.id.horizontalRv);
 
         //save data source
@@ -82,7 +82,7 @@ public class FirstFragment extends Fragment {
         dataSource.add("Travelling by bike or foot emits a fraction of the CO2 *of a car*.");
         dataSource.add("Fossil fuel account for around 80-90 per cent of our CO2 emissions.");
         dataSource.add("Buildings emit 39 percent of the CO2 **released in the US**.");
-        dataSource.add("The more paper you use,the more you are contributing to carbon emissions.");
+        dataSource.add("More paper you use, more you are contributing to carbon emissions.");
         dataSource.add("On the scale of CO2 emissions,human sources far outweigh volcanoes.");
 
 //        linearLayoutManager = (new LinearLayoutManager(getActivity()));
@@ -93,8 +93,27 @@ public class FirstFragment extends Fragment {
         rv.setLayoutManager(linearLayoutManager);
         rv.setAdapter(myRvAdapter);
 
+        //vertical recyclerview
+        rv2 = view.findViewById(R.id.verticalRv);
+
+        //save data source
+        dataSource = new ArrayList<>();
+        dataSource.add("20km trip in sedan emits 20% carbon");
+        dataSource.add("4.5km trip in SUV emits 13% carbon");
+        dataSource.add("6.1km trip in motorbike emits 5% carbon");
+
+
+        linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+
+        myRvAdapter2 = new MyRvAdapter2(dataSource);
+        rv2.setLayoutManager(linearLayoutManager);
+        rv2.setAdapter(myRvAdapter2);
+
+
+
         return view;
     }
+
 
     class MyRvAdapter extends RecyclerView.Adapter<MyRvAdapter.MyHolder>{
         ArrayList<String> data;
@@ -133,6 +152,46 @@ public class FirstFragment extends Fragment {
         }
 
     }
+
+
+    class MyRvAdapter2 extends RecyclerView.Adapter<MyRvAdapter2.MyHolder2>{
+        ArrayList<String> data2;
+
+        public MyRvAdapter2(ArrayList<String> data) {
+            this.data2 = data;
+        }
+
+        @NonNull
+        @Override
+        public MyHolder2 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//            View view = LayoutInflater.from(FirstFragment.this).inflate(R.layout.rv_item, parent, false);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_list, parent, false);
+            return new MyHolder2(view);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data2.size();
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyHolder2 holder, int position) {
+            holder.recentRecord.setText(data2.get(position));
+
+        }
+
+        class MyHolder2 extends RecyclerView.ViewHolder{
+
+            TextView recentRecord;
+
+            public MyHolder2(@NonNull View itemView) {
+                super(itemView);
+                recentRecord = itemView.findViewById(R.id.recentRecord);
+            }
+        }
+
+    }
+
 
 
 
